0cb1eb48e8a2e04e4a8c58b8502b3196951959e4
hector the protector <hmanzanodiaz@gmail.com>
2020-05-27 21:24:02 -0700

    complete Control flow + functions exercise

--- README.txt
+++ README.txt
@@ -7,5 +7,13 @@ These are the GIT steps:
 4. Check status --> git status
 5. git commit -m "Start commit steps"
 6. check logs --> git log
+7. <git push -u origin master> to commit on github
 
 
+first time Repository and File
+1. mkdir git_practice to make a new directory to practice.
+2. cd git_practice to make the new directory your working directory.
+3. git init to turn the current, empty directory into a fresh Git repository.
+4. echo "Hello Git and GitHub" >> README.txt to create a new README file (more on this later) with some sample text.
+5. git add README.txt to add the new file to the Git staging area.
+6. git commit -m "First commit" to make your first commit with the new README file.
---
+++ sals_shipping.py
@@ -0,0 +1,80 @@
+# # create ground shipping function
+# # create drone shipping function
+# # create premium shipping
+# #objective: find out weight and offer cheapest option within shipping classes
+#
+# BELOW THE INITIAL CODE THAT RETURN IN ERROR ----------------------------------------
+
+# def ground_shipping(weight):
+#   if weight <= 2:
+#     cost = weight * 1.50 + 20
+#   elif (weight > 2) and (weight <= 6):
+#     cost = weight * 3.00 + 20
+#   elif (weight > 6) and (weight <= 10):
+#     cost = weight * 4.00 + 20
+#   elif weight > 10:
+#     cost = weight * 4.75 + 20
+#   else:
+#     print("wrong weight")
+#   return "$" + str(cost) + " with ground shipping"
+#   print("Ground Shipping is the cheapest")
+#
+#
+# def drone_shipping(weight):
+#   if weight <= 2:
+#     cost = weight * 4.50
+#   elif (weight > 2) and (weight <= 6):
+#     cost = weight * 9.00
+#   elif (weight > 6) and (weight <= 10):
+#     cost = weight * 12.00
+#   elif weight > 10:
+#     cost = weight * 14.25
+#   else:
+#     print("wrong weight")
+#   return "$" + str(cost)
+#   print("Drone Shipping is the cheapest")
+#
+# print(ground_shipping(-40))
+# print(drone_shipping(7))
+
+# create ground shipping function
+# create drone shipping function
+# create premium shipping
+#objective: find out weight and offer cheapest option within shipping classes
+
+
+def ground_shipping(weight):
+  if weight <= 2:
+    cost = 1.50
+  elif (weight > 2) and (weight <= 6):
+    cost = 3.00
+  elif (weight > 6) and (weight <= 10):
+    cost = 4.00
+  elif weight > 10:
+    cost = 4.75
+  return (weight * cost) + 20
+
+
+def drone_shipping(weight):
+  if weight <= 2:
+    cost = 4.50
+  elif (weight > 2) and (weight <= 6):
+    cost = 9.00
+  elif (weight > 6) and (weight <= 10):
+    cost = 12.00
+  elif weight > 10:
+    cost = 14.25
+  return cost * weight
+
+def all_shipping(weight):
+  premium = 125.00
+  ground = ground_shipping(weight)
+  drone = drone_shipping(weight)
+  if ground < drone and ground < premium:
+    print("Ground is the cheapest")
+  elif drone < premium and drone < ground:
+    print("drone is the cheapest")
+  elif premium < ground and premium < drone:
+    print("Premium is the cheapest")
+
+print(all_shipping(80))
