3dff30fc3c42b947fd49844a64d3235839b59472
hector the protector <hmanzanodiaz@gmail.com>
2020-06-07 08:16:43 -0700

    add same value exercice

--- lists_exercices.py
+++ lists_exercices.py
@@ -290,3 +290,32 @@ def max_num(nums):
 
 #Uncomment the line below when your function is done
 print(max_num([50, -10, 0, 75, 20]))
+
+# 13
+
+# equals = [a for a in lst1 for b in lst2 if a == b]
+#  return equals
+
+# def same_values(lst1, lst2):
+#     equals = []
+#     for a in lst1:
+#       for b in lst2:
+#         if a == b:
+#           equals.append(a)
+#     return equals
+
+
+#Write your function here
+def same_values(lst1, lst2):
+  new_lst = []
+  for index in range(len(lst1)): # range() and len() looks at the index
+    if lst1[index] == lst2[index]:
+      new_lst.append(index)
+  return new_lst
+
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+
+
+
