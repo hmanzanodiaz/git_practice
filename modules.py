366f6f8df199b7eb13873300bcd2c2526ca0c403
hector the protector <hmanzanodiaz@gmail.com>
2020-07-13 16:23:34 -0700

    new module exercices

---
+++ modules.py
@@ -0,0 +1,14 @@
+# Import random below:
+import random
+
+# Create random_list below:
+random_list = []
+
+# Create randomer_number below:
+
+random_list = [random.randint(0,101) for x in range(0, 101)]
+
+# Print randomer_number below:
+randomer_number = random.choice(random_list)
+
+print(randomer_number)
